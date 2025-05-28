c0e6daa94729046d783374028b29c807282a5a4c
Battlebee12 <Sarab4space@gmail.com>
2025-05-28 13:36:38 -0700

    Init

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+// Users route
+const usersRouter = require('./routes/users');
+app.use('/api/users', usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
