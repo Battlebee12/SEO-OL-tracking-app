651ba0ae7b10b4c3e7eee7dc26d00a00f0459263
Battlebee12 <Sarab4space@gmail.com>
2025-06-03 10:49:56 -0700

    admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import Home from './pages/Home.jsx';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+// import OLCheckInOut from "./ShiftForm";
+import AdminDashboard from "./components/AdminDashboard";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/shift-form" element={<OLCheckInOut />} /> */}
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
